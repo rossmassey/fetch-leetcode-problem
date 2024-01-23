f449be61b971467575bf43e0d077de6c552592a8
Ross Massey <massey0ross@gmail.com>
2024-01-22 23:25:32 -0800

    print functions in main

--- src/main.py
+++ src/main.py
@@ -15,8 +15,7 @@ def main():
             try:
                 num = int(argument)
                 info = get(num)
-                for key, value in info.items():
-                    print(f'{key:<{15}} {value}')
+                print_info(info)
 
             except ValueError:
                 print(f'Invalid argument: {argument}')
@@ -29,8 +28,28 @@ def main():
 def help():
     print('Usage:')
     print('')
-    print('main.py u        -- update the problem index')
-    print('main.py <num>    -- get problem info for <num>')
+    print('main.py u     -- update the problem index')
+    print('main.py <num> -- get problem info for <num>')
+
+
+def print_info(info):
+    title = f'{info['num']} - {info['title']} - {info['difficulty']}'
+    print(title)
+    print('=' * len(title))
+    print(info['description'])
+    [print_example(example) for example in info['examples']]
+    print('Constraints:')
+    [print(f'- {constraint}') for constraint in info['constraints']]
+
+    print('\nraw:\n', info)
+
+
+def print_example(example):
+    print(f'Example {example['n']}')
+    print(f'\tInput: {example['input']}')
+    print(f'\tOutput: {example['output']}')
+    print(f'\tImg: {example['img']}')
+    print(f'\tExplanation: {example['explanation']}')
 
 
 if __name__ == '__main__':
